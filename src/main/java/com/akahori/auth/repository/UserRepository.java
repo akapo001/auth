db43c83b55486d8a30063e638013ac7cc9662643
akahorisoma <akapo.kk.i@gmail.com>
2020-04-17 18:55:44 +0900

    refactor xml設定からアノテーション設定に変更

--- src/main/java/com/akahori/auth/repository/UserRepository.java
+++ src/main/java/com/akahori/auth/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.akahori.auth.repository;
 
 import com.akahori.auth.model.UserModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Insert;
 
 @Mapper
 public interface UserRepository {
+
+    @Select("SELECT id, name, password, enabled FROM t_user where id = #{id}")
     public UserModel selectByUser(String username);
 
+    @Insert("INSERT INTO t_user(id,name,password,enabled) VALUES (#{id}, #{name}, #{password}, #{enabled})")
     public void save(UserModel userModel);
 }
